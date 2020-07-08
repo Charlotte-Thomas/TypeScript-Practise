7ad88388a894566d1f449656ef5bbbe3a2a9801f
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-07-08 14:56:09 +0100

    comments added

--- Basics/main.js
+++ Basics/main.js
@@ -1,4 +1,6 @@
 "use strict";
+// run: tsc main --watch 
+// in terminal so any changes are auto complied into main.js
 exports.__esModule = true;
 var message = 'hello world';
 console.log(message);
--- Basics/main.ts
+++ Basics/main.ts
@@ -1,4 +1,7 @@
-export {}
+// run: tsc main --watch 
+// in terminal so any changes are auto complied into main.js
+
+export {} //export empty brackets so that TS treats the file as a module not a script
 
 let message = 'hello world'
 console.log(message)
