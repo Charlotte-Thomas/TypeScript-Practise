da1112cd07b7128f89aae6f6da56d7d5c518920b
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-07-08 15:10:31 +0100

    comments added

--- Basics/main.js
+++ Basics/main.js
@@ -4,3 +4,10 @@
 exports.__esModule = true;
 var message = 'hello world';
 console.log(message);
+var isBeginner = true;
+var total = 0;
+var name = 'Charlotte';
+var sentence = "my name is " + name + "\ni am new to TypeScript";
+console.log(sentence);
+// TS will help by only showing intellisense related to the TYPE 
+// e.g. writing name.  will give options related to string
--- Basics/main.ts
+++ Basics/main.ts
@@ -5,3 +5,15 @@ export {} //export empty brackets so that TS treats the file as a module not a s
 
 let message = 'hello world'
 console.log(message)
+
+let isBeginner: Boolean = true
+let total: number = 0
+let name: string = 'Charlotte'
+
+let sentence: string = `my name is ${name}
+i am new to TypeScript`
+console.log(sentence)
+
+
+// TS will help by only showing intellisense related to the TYPE 
+// e.g. writing name.  will give options related to string
