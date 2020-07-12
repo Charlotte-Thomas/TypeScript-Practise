1d51b06f4605c251f3f04fe2e81416f807e0680b
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-07-12 15:11:43 +0100

    home modified

--- React-TS/src/components/Home.tsx
+++ React-TS/src/components/Home.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
 import { Link } from 'react-router-dom'
 
-const Home = () => (
+export interface HelloProps { compiler: string; framework: string; }
+
+const Home = (props: HelloProps) => (
   <section className="home">
-    <p> home </p>
+    <h1>Hello from {props.compiler} and {props.framework}!</h1>
   </section>
 )
 
 export default Home
+
