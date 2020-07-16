58ec3a9ff0bbad64b3e05ce992c2a38b79ec4f68
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-07-16 14:26:46 +0100

    name added

--- React-TS/src/components/Home.tsx
+++ React-TS/src/components/Home.tsx
@@ -3,11 +3,17 @@ import { Link } from 'react-router-dom'
 
 export interface HelloProps { compiler: string; framework: string; }
 
-const Home = (props: HelloProps) => (
-  <section className="home">
+const Home = (props: HelloProps) => {
+
+  const name: string = 'Charlotte'
+
+  return (
+  <section className="home" >
     <h1>Hello from {props.compiler} and {props.framework}!</h1>
-  </section>
-)
+    <p>{name}</p>
+  </section >
+  )
+}
 
 export default Home
 
