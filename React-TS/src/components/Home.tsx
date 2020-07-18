b9460e99dba09be238b90554f41b7cfca3c63fe1
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-07-18 18:03:17 +0100

    steValues func

--- React-TS/src/components/Home.tsx
+++ React-TS/src/components/Home.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export interface HelloProps { compiler: string; framework: string; }
 
@@ -7,20 +8,28 @@ const Home = (props: HelloProps) => {
 
   const name: string = 'Charlotte'
 
-  const [num1, setnum1]: [number, number] = useState(0)
+  const [num1, setnum1]: [number, any] = useState(0)
+  const [num2, setnum2]: [number, any] = useState(0)
+
+  function setValues(e: any, num: number) {
+      const val: number = parseInt(e.target.value)
+      if (val) {
+        num === 1 ? setnum1(val) : setnum2(val)
+      }
+  }
 
   return (
-  <section className="home" >
-    <h1>Hello from {props.compiler} and {props.framework}!</h1>
-    <p>{name}</p>
-    <form>
-      <input placeholder='Enter a number'></input>
-      <br /> + <br />
-      <input placeholder='Enter another number'></input>
-      <br />
+    <section className="home" >
+      <h1>Hello from {props.compiler} and {props.framework}!</h1>
+      <p>{name}</p>
+      <form>
+        <input onChange={(e) => setValues(e, 1)} placeholder='Enter a number'></input>
+        <br /> + <br />
+        <input placeholder='Enter another number'></input>
+        <br />
       =
     </form>
-  </section >
+    </section >
   )
 }
 
