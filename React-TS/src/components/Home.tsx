9c5989ff46fa35ac930e7eb6d0d1be0e57a8c14c
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-07-18 17:42:50 +0100

    inputs added

--- React-TS/src/components/Home.tsx
+++ React-TS/src/components/Home.tsx
@@ -7,10 +7,19 @@ const Home = (props: HelloProps) => {
 
   const name: string = 'Charlotte'
 
+  const [num1, setnum1]: [number, number] = useState(0)
+
   return (
   <section className="home" >
     <h1>Hello from {props.compiler} and {props.framework}!</h1>
     <p>{name}</p>
+    <form>
+      <input placeholder='Enter a number'></input>
+      <br /> + <br />
+      <input placeholder='Enter another number'></input>
+      <br />
+      =
+    </form>
   </section >
   )
 }
