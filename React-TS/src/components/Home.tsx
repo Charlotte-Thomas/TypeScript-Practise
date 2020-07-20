cde85e6ef7e600e83df5a9c862179c8bbef6fb4d
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-07-20 16:16:45 +0100

    calculator made

--- React-TS/src/components/Home.tsx
+++ React-TS/src/components/Home.tsx
@@ -10,6 +10,7 @@ const Home = (props: HelloProps) => {
 
   const [num1, setnum1]: [number, any] = useState(0)
   const [num2, setnum2]: [number, any] = useState(0)
+  const [val, setval]: [number, any] = useState(0)
 
   function setValues(e: any, num: number) {
       const val: number = parseInt(e.target.value)
@@ -18,16 +19,22 @@ const Home = (props: HelloProps) => {
       }
   }
 
+  function getVal(e: any) {
+    e.preventDefault()
+    setval(num1 + num2)
+  }
+
   return (
     <section className="home" >
       <h1>Hello from {props.compiler} and {props.framework}!</h1>
       <p>{name}</p>
-      <form>
+      <form onSubmit={(e) => getVal(e)}>
         <input onChange={(e) => setValues(e, 1)} placeholder='Enter a number'></input>
         <br /> + <br />
-        <input placeholder='Enter another number'></input>
+        <input onChange={(e) => setValues(e, 2)} placeholder='Enter another number'></input>
         <br />
-      =
+        <button>enter</button>
+      = {val}
     </form>
     </section >
   )
